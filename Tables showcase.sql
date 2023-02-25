7c5847dac9fc0fa234f86a62857dda9fb67a9611
drigger091 <42235258+drigger091@users.noreply.github.com>
2023-02-25 16:35:47 +0530

    project update

---
+++ Tables showcase.sql
@@ -0,0 +1,7 @@
+use BikeStore;
+select * from sales.orders;
+select * from production.brands;
+select * from production.categories;
+select * from sales.customers;
+select * from sales.orders;
+select * from sales.order_items; 
